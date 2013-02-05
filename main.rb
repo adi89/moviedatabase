99218e6c5cac139ad9bcc2960d70a17a620ea3a0
Joseph Waine <joe.waine@gmail.com>
2013-02-04 21:41:53 -0500

    added code

--- main.rb
+++ main.rb
@@ -1,1 +1,29 @@
-# this is da movie database
+
+require 'httparty'
+require 'JSON'
+require 'pry'
+
+conn = PG.connect(:dbname =>'moviez', :host => 'localhost')
+conn.exec( "select * from film" ) do |result|
+  result.each do |row|
+  end
+end
+
+composite = "http://www.omdbapi.com/?i=&t="
+puts 'type the movie name you would like to look up.'
+ft = gets.split.join("+").to_s
+
+mov_name = composite + ft
+
+input = HTTParty.get(mov_name)
+
+names = JSON(input.body)
+
+
+puts "title: #{names.values.join("\n")}"
+# puts "shit, an error."
+
+# puts "{names.values}"
+
+
+
